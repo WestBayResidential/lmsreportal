f7bc682268868478f2d2c3091193fe11bd079142
Paul LaRiviere <plariv@augurynet.com>
2017-11-27 08:56:44 -0500

    Initial pass at archived certificates controller, aka CertzArch

---
+++ app/Http/Controllers/CertzArchController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+// CERTZ application lists archived certificates from legacy WBLMS
+//
+class CertzArchController extends Controller
+{
+    public function select()
+    {
+        //dd(request()->all());
+        
+
+    }
+
+
+}
+
